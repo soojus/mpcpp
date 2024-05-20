153b69a04b02795aaf281861b322b23e75acf55c
edgegg <ske4715@gmail.com>
2024-05-21 08:59:55 +0900

    cin

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Hello World!!\n";
-    std::cout<<"안녕하세요"<<std::endl;
+    cout << "너비를 입력하세요: ";
+    int width;
+    cin >> width;
+
+    cout << "높이를 입력하세요: ";
+    int height;
+    cin >> height;
+
+    cout << "면적: " << width * height << endl;
     return 0;
 }
-
