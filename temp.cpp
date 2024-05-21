572ecc5084c088b58912aac757dac0effc2695be
andrew <kihan@snut.ac.kr>
2024-05-21 09:52:48 +0900

    머지후 제거

--- temp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char const *argv[])
-{
-    std::cout<<"Where are you going now?\n";
-    std::cout<<"I have to go"<<std::endl;
-    std::cout<<"my home now."<<std::endl;
-    return 0;
-}
