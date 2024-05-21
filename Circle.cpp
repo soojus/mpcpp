aa71b9bb25b2bb794862e6f5e6f3a2658dfe28ae
andrew <kihan@snut.ac.kr>
2024-05-21 15:20:19 +0900

    예제 3-1 Circle 클래스 - 선언과 정의로 파일 분리함

---
+++ Circle.cpp
@@ -0,0 +1,6 @@
+#include "Circle.h"
+
+Circle::Circle() {radius=1;}
+Circle::Circle(int r)   {radius=r;}
+int Circle::getRadius() { return radius; }
+double Circle::getArea() { return 3.14 * radius * radius; }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
+#include "Circle.h"
 using namespace std;
-
-class Circle
-{
-private:
-    int radius;
-public:
-    Circle();
-    Circle(int r);
-    int getRadius();
-    double getArea();
-};
-
-Circle::Circle() {radius=1;}
-Circle::Circle(int r)   {radius=r;}
-int Circle::getRadius() { return radius; }
-double Circle::getArea() { return 3.14 * radius * radius; }
-
-
-
-
 
 int main(int argc, char const *argv[])
 {
