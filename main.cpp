c797f45856fe961adb6b4ca0ecd2eefca5f892c6
edgegg <ske4715@gmail.com>
2024-05-21 10:49:12 +0900

    getline

--- main.cpp
+++ main.cpp
@@ -4,14 +4,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "너비를 입력하세요: ";
-    int width;
-    cin >> width;
-
-    cout << "높이를 입력하세요: ";
-    int height;
-    cin >> height;
-
-    cout << "면적: " << width * height << endl;
+    cout<<"주소를 입력하세요: ";
+    char address[100];
+    cin.getline(address,sizeof(address),'\n');
+    cout << "주소는 " << address << "입니다\n";
     return 0;
 }
