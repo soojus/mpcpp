d65fa3172fff8e7ff92383d2d4cd215f628a778b
andrew <kihan@snut.ac.kr>
2024-05-21 14:15:48 +0900

    예제2-7

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"주소를 입력하세요: ";
-    char address[100];
-    cin.getline(address, 100,'\n');
-    cout<<"주소: "<<address<<endl;
+    string song("Falling in love with you");
+    string elvis("Elvis Presley");
+    string singer;
+
+    
+    cout<<song + "를 부른 가수는";
+    cout<<"(힌트: 첫글자는)"<<elvis[0] <<  ")";
+    //cin.getline(singer, 20);
+    getline(cin, singer);
+    if(singer==elvis)
+        cout<<"Elvis!";
+    else
+        cout<<"Elvis 가 아닙니다. !";
 
     return 0;
 }
