6688f1b422a90e95504a9fbc6f154e52865f13ea
edgegg <ske4715@gmail.com>
2024-05-21 15:52:44 +0900

    .

--- Circle.cpp
+++ Circle.cpp
@@ -1,5 +1,11 @@
 #include "Circle.h"
+#include <iostream>
+
 Circle::Circle() { radius = 1; }
+Circle::~Circle()
+{
+    std::cout<<"Destroy Circle"<<std::endl;
+}
 Circle::Circle(int r) { radius = r; }
 int Circle::getRadius() { return radius; }
 double Circle::getArea() { return 3.14 * radius * radius; }
