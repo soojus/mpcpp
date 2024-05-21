efc86cca93d4747db8a3d426fdd01ed1ab62054d
edgegg <ske4715@gmail.com>
2024-05-21 16:43:45 +0900

    .

--- Circle.h
+++ Circle.h
@@ -8,4 +8,16 @@ public:
     Circle(int r);
     int getRadius();
     double getArea();
+protected:
+    void setRadius(int r);
+};
+
+class Ecllipse : public Circle{
+private:
+    int radius=0;
+public:
+    Ecllipse(int r, int w);
+    double getArea();
+    void setWidth(int w);
+    ~Ecllipse();
 };
