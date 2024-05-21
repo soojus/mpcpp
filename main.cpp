63453e090857892f8ad32bea1e93fff07c98211a
Hyun <gusdn9706@gmail.com>
2024-05-21 09:41:44 +0900

    main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char const *argv[])
     cout<<"안녕하세요"<<std::endl;
     cout<<"반갑습니다."<<std::endl;
     cout<<"Hyun."<<std::endl;
+
+    char address[100];
+    cin.getline(address,100);
+    cout<<address<<endl;
     
     return 0;
 }
