ae4ac21af6b8267526302bd44e16b185869db83e
andrew <kihan@snut.ac.kr>
2024-05-22 10:04:43 +0900

    4-1 객체 포인터- new, delete

--- main.cpp
+++ main.cpp
@@ -6,18 +6,23 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     {
-        Circle donut;
-        Circle pizza(30);
+        Circle *donut= new Circle();
+
+        Circle *pizza=new Circle(30);
         Ecllipse e(10, 20);
-        cout << pizza.getArea() << endl;
-        cout << donut.getArea() << endl;
+        cout << pizza->getArea() << endl;
+        cout << donut->getArea() << endl;
         cout << e.getArea() << endl;
         
+        delete pizza;
+        delete donut;   
     }
-    Rectangle box(4, 40);
-    Rectangle phone(10, 20);
-    cout << box.getArea() << endl;
-    cout << phone.getArea() << endl;
+    Rectangle *box=new Rectangle(4, 40);
+    Rectangle *phone=new Rectangle(10, 20);
+    cout << box->getArea() << endl;
+    cout << phone->getArea() << endl;
 
+    delete box;
+    delete phone;
     return 0;
 }
