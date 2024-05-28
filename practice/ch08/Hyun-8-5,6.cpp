c8645f0b50cd5fc199b2e41c58383f4c6820d43a
Hyun <gusdn9706@gmail.com>
2024-05-28 12:02:33 +0900

    8장

--- practice/ch08/Hyun-8-5,6.cpp
+++ practice/ch08/Hyun-8-5,6.cpp
@@ -27,15 +27,45 @@ class BaseArray{
 
 class MyQueue : public BaseArray{
     private :
+        int arraynum;
     public :
         MyQueue(int capacity) : BaseArray(capacity){
-
+            this->arraynum =0;
         }
         void enqueue(int val){
-            put(getCapacity()-1, val);
+            put(arraynum++, val);
+        }
+        int qCap()
+        {
+            return getCapacity();
+        }
+        int length()
+        {
+            return arraynum;
+        }
+        int dequeue()
+        {
+            int returnQ = get(0);
+            for (int i = 1; i < arraynum; ++i) {
+                put(i - 1, get(i));
+            }
+            arraynum--;
+            return returnQ;
         }
 };
 int main()
 {
-
+    MyQueue mQ(100);
+    int n;
+    cout << "큐에 삽입할 5개의 정수를 입력하라 >>";
+    for(int i = 0; i < 5; i++){
+        cin >> n;
+        mQ.enqueue(n);
+    }
+    cout << "큐의 용량 : " << mQ.qCap() << " 큐의 크기 : "<< mQ.length()<<endl;
+    while(mQ.length() != 0){
+        cout << mQ.dequeue() << ' ';
+    }
+    cout << endl << "큐의 현재크기 : " << mQ.length() <<    endl;
+    return 0;
 }
